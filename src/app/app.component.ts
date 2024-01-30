77a05881becf27b58b837a98491ca65322dec648
Alexander-Calle <ajcalle@sudamericano.edu.ec>
2024-01-29 21:05:00 -0500

    Componentes e imagenes agregadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,24 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { OurCoursesComponent } from './our-courses/our-courses.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    WelcomeComponent,
+    AboutUsComponent,
+    OurCoursesComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'paginaColegio';
