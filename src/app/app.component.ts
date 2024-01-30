1853d4e40d39be76cfb5ba7a073cbb92dc993993
Alexander-Calle <ajcalle@sudamericano.edu.ec>
2024-01-29 20:36:28 -0500

    Diseño dividido en componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,24 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { OurCoursesComponent } from './our-courses/our-courses.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    AboutUsComponent,
+    OurCoursesComponent,
+    WelcomeComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'paginaColegio';
